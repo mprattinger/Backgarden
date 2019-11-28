b76705121ae064277bcca2aec6985ff0a395ec45
Michael Prattinger <mprattinger@outlook.com>
2019-11-28 20:28:40 +0100

    removed error

--- src/Startup.cs
+++ src/Startup.cs
@@ -29,8 +29,6 @@ namespace backgarden
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
-
-            services.AddMvb();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
