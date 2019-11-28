6727f2d325eea1e4cf99a49b3c679c1c65051bfa
Michael Prattinger <mprattinger@outlook.com>
2019-11-28 20:26:52 +0100

    with error

--- src/Startup.cs
+++ src/Startup.cs
@@ -29,6 +29,8 @@ namespace backgarden
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddMvb();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
